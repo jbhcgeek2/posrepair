ff899d515828366dcdbd2b4d77f56b22ede9d06d
jbhcgeek2 <joelbh92@gmail.com>
2024-04-27 12:00:25 -0700

    Ventas del dia

--- reportesCaja.php
+++ reportesCaja.php
@@ -47,14 +47,14 @@ session_start();
       
                     <div class="row">
                       
-                      <div class="col-sm-6 col-md-4 col-lg-3 mb-sm-0">
-                        <div class="card">
-                          <div class="card-body">
-                            <h5 class="card-title">Hola Mundo</h5>
-                            
-                          </div>
-                        </div>
-                      </div>
+                    <div class="col-6 col-lg-3">
+                      <div class="app-card app-card-stat shadow-sm h-100">
+                        <div class="app-card-body p-3 p-lg-4">
+                          <h4 class="stats-type mb-1">Ventas del dia</h4>
+                        </div><!--//app-card-body-->
+                        <a class="app-card-link-mask" href="#"></a>
+                      </div><!--//app-card-->
+                    </div><!--//col-->
 
                     </div>
 
