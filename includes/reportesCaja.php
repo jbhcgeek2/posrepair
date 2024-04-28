9a4578c01a9c14d280b155cf704654b64768a570
jbhcgeek2 <joelbh92@gmail.com>
2024-04-28 16:23:53 -0700

    Implementacion Reporte Salidas y Entradas de efectivo 8

--- includes/reportesCaja.php
+++ includes/reportesCaja.php
@@ -85,7 +85,7 @@ if(!empty($_SESSION['usuarioPOS'])){
     $fechaIniMov = $_POST['fechaIniMov'];
     $fechaFinMov = $_POST['fechaFinMov'];
     $sql = "";
-    echo $rolUsuario;
+    echo $idEmpresaSesion;
 
     if($rolUsuario == "Administrador"){
       $sql = "SELECT *,(SELECT b.userName FROM USUARIOS b WHERE b.idUsuario = a.usuarioMov) AS usmov,
