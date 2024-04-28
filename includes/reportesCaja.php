39e383f081b787a631c80467d53b814a41b79505
jbhcgeek2 <joelbh92@gmail.com>
2024-04-27 17:09:54 -0700

    Nueva correccion sql

--- includes/reportesCaja.php
+++ includes/reportesCaja.php
@@ -39,19 +39,19 @@ if(!empty($_SESSION['usuarioPOS'])){
       $sql = "SELECT * FROM DETALLEVENTA a INNER JOIN VENTAS b ON a.ventaID = b.idVenta 
       INNER JOIN ARTICULOS c ON a.articuloID = c.idArticulo 
       INNER JOIN SUCURSALES d ON a.sucursalID = d.idSucursal
-      WHERE b.fechaVenta BETWEEN $fechaIni AND $fechaFin";
+      WHERE b.fechaVenta BETWEEN '$fechaIni' AND '$fechaFin'";
     }elseif($rolUsuario == "Vendedor"){
       //solo podra ver las ventas de su usuario y sucursal
       $sql = "SELECT * FROM DETALLEVENTA a INNER JOIN VENTAS b ON a.ventaID = b.idVenta 
       INNER JOIN ARTICULOS c ON a.articuloID = c.idArticulo
-      WHERE (b.fechaVenta BETWEEN $fechaIni AND $fechaFin) AND a.usuarioVenta = '$usuario' 
+      WHERE (b.fechaVenta BETWEEN '$fechaIni' AND '$fechaFin') AND a.usuarioVenta = '$usuario' 
       AND a.sucursalID = '$idSucursalN'";
     }else{
       //el usuario encargado podra ver las ventas de todos
       //los usuarios, pero solo de su susucrsal
       $sql = "SELECT * FROM DETALLEVENTA a INNER JOIN VENTAS b ON a.ventaID = b.idVenta 
       INNER JOIN ARTICULOS c ON a.articuloID = c.idArticulo
-      WHERE(b.fechaVenta BETWEEN $fechaIni AND $fechaFin) AND a.sucursalID = '$idSucursalN'";
+      WHERE(b.fechaVenta BETWEEN '$fechaIni' AND '$fechaFin') AND a.sucursalID = '$idSucursalN'";
     }
 
     try {
