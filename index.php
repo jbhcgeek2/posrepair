9906feb65a9c430860a2147c945c0eadee23fec8
jbhcgeek2 <joelbh92@gmail.com>
2024-05-04 13:01:52 -0700

    Se agrega enlace a productos por agotarse

--- index.php
+++ index.php
@@ -343,7 +343,7 @@
 																	<td class='$claseInve'>$prod</td>
 																	<td class='stat-cell $claseInve'>$nombreSuc</td>
 																	<td class='stat-cell $claseInve'>
-																		<a href='verInfoProducto.php?infoProd=$idProdInve'>
+																		<a href='verInfoProducto.php?infoProd=$idProdInve' class='$claseInve'>
 																			$existencia
 																		</a>
 																	</td>
