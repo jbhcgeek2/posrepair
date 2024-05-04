a034d1f9f03b99898407edec44123f859f4353c9
jbhcgeek2 <joelbh92@gmail.com>
2024-05-04 14:49:04 -0700

    Se agrega categorias al navbar

--- includes/header.php
+++ includes/header.php
@@ -328,6 +328,28 @@
 								</li>
 
 								<li class="nav-item has-submenu">
+									<a href="#!" class="nav-link submenu-toggle" data-bs-toggle="collapse" data-bs-target="#submenu-categorias" aria-expanded="false" aria-controls="submenu-categorias">
+										<span class="nav-icon">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-diagram-2-fill" viewBox="0 0 16 16">
+												<path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 5 7h2.5V6A1.5 1.5 0 0 1 6 4.5zm-3 8A1.5 1.5 0 0 1 4.5 10h1A1.5 1.5 0 0 1 7 11.5v1A1.5 1.5 0 0 1 5.5 14h-1A1.5 1.5 0 0 1 3 12.5zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1A1.5 1.5 0 0 1 9 12.5z"/>
+											</svg>
+										</span>
+										<span class="nav-link-text">Categorias</span>
+										<span class="submenu-arrow">
+											<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+													<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+												</svg>
+	                	</span><!--//submenu-arrow-->
+									</a>
+									<div id="submenu-categorias" class="collapse submenu submenu-categorias" data-bs-parent="#menu-accordion">
+										<ul class="submenu-list list-unstyled">
+											<li class="submenu-item"><a class="submenu-link" href="verUsuarioEmpr.php">Ver Categorias</a></li>
+											<li class="submenu-item"><a class="submenu-link" href="altaCategoria.php">Registrar Categorias</a></li>
+										</ul>
+									</div>
+								</li>
+
+								<li class="nav-item has-submenu">
 									<a href="#!" class="nav-link submenu-toggle" data-bs-toggle="collapse" data-bs-target="#submenu-usuarios" aria-expanded="false" aria-controls="submenu-cliinventarioente">
 										<span class="nav-icon">
 											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-gear" viewBox="0 0 16 16">
--- index.php
+++ index.php
@@ -415,21 +415,22 @@
 												$nombreProd = $fetch7['nameArti'];
 												$ventasProd = $fetch7['totales'];
 												$porcentaje = ($ventasProd / $totalesVentas) * 100;
+												$porcentaje2 = number_format($porcentaje,2);
 												echo '
 												<div class="item p-3">
 													<div class="row align-items-center">
 														<div class="col">
-															<div class="title mb-1 ">'.$nombreProd.' - '.$ventasProd.'</div>
+															<div class="title mb-1 ">'.$nombreProd.' - '.$porcentaje2.'%</div>
 															<div class="progress">
 																<div class="progress-bar bg-success" role="progressbar" style="width: '.$porcentaje.'%;" aria-valuenow="'.$porcentaje.'" aria-valuemin="0" aria-valuemax="100"></div>
 															</div>
 														</div><!--//col-->
 
-														<div class="col-auto">
+														<!--<div class="col-auto">
 															<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 																<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
 															</svg>
-														</div><!--//col-->
+														</div>-->
 													</div><!--//row-->
 
 													<a class="item-link-mask" href="#!"></a>
