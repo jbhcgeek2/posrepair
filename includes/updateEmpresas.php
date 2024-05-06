7b1beba6aca93aea3bf467956e950df1e53f3dc6
jbhcgeek2 <joelbh92@gmail.com>
2024-05-06 15:38:43 -0700

    Se agregan metodo para actualizar empresa

--- includes/updateEmpresas.php
+++ includes/updateEmpresas.php
@@ -4,17 +4,80 @@ session_start();
 
 if(!empty($_SESSION['usuarioPOS'])){
   include('conexion.php');
+  include("usuarios.php");
+  include("documentos.php");
+
+
+  $usuario = $_SESSION['usuarioPOS'];
+  $empresa = datoEmpresaSesion($usuario,"id");
+  $empresa = json_decode($empresa);
+  $idEmpresaSesion = $empresa->dato;
 
   if(!empty($_POST['nombreEmpresa'])){
     $nombreEmpresa = $_POST['nombreEmpresa'];
     $suscripcion = $_POST['planEmpresa'];
 
-    //verificamos si se subio imagen para procesarla
-    if(!empty($_FILES['logotipo']['tmp_name'])){
-      echo "tiene logo";
+    //antes de continuar, verificamos si cambio de plan
+    $sqlEmp = "SELECT * FROM EMPRESAS WHERE idEmpresa = '$idEmpresaSesion'";
+    try {
+      $queryEmp = mysqli_query($conexion, $sqlEmp);
+      if(mysqli_num_rows($queryEmp) == 1){
+        $fetchEmp = mysqli_fetch_assoc($queryEmp);
+        $susActual = $fetchEmp['suscripcionID'];
+        $sqlUpdate = "";
+        if($suscripcion != $susActual){
+          //se actualizara el plan
+          $sqlUpdate = "UPDATE EMPRESAS SET nombreEmpresa = '$nombreEmpresa', 
+          nuevaSuscripcion = '$suscripcion', estatusNuevaSus = 'Espera' 
+          WHERE idEmpresa = '$idEmpresaSesion'";
+        }else{
+          //no se actualiza el plan
+          $sqlUpdate = "UPDATE EMPRESAS SET nombreEmpresa = '$nombreEmpresa' 
+          WHERE idEmpresa = '$idEmpresaSesion'";
+        }
+        //aplicamos el update
+        try {
+          $queryUpdate = mysqli_query($conexion, $sqlUpdate);
+          //ahora verificamos si se actualiza el logotipo
+          //verificamos si se subio imagen para procesarla
+          if(!empty($_FILES['logotipo']['tmp_name'])){
+            //verificamos el tipo de imagen
+            $tmpFile = $_FILES['logotipo']['tmp_name'];
+            $nombreFile = "logotipo_".$idEmpresaSesion;
+            $ruta = "imgEmpr/".$idEmpresaSesion;
+            $subir = uploadDoc($tmpFile,'imagen',$nombreFile,$ruta,$idEmpresaSesion);
+            $subir = json_decode($subir);
+            if($subir->estatus == "ok"){
+              $res = ["status"=>"ok","mensaje"=>"operationComplete"];
+              echo json_encode($res);
+            }else{
+              //error al subir la imagen
+              $err = $subir->mensaje;
+              $res = ["status"=>"error","mensaje"=>$err];
+              echo json_encode($res);
+            }
+          }else{
+            //no tiene logo terminamos el proceso
+            $res = ["status"=>"ok","mensaje"=>"operationComplete"];
+            echo json_encode($res);
+          }
+          
+        } catch (\Throwable $th) {
+          //error al actualizar 
+          $res = ["status"=>"error","mensaje"=>"Ha ocurrido un error al actualizar los datos de la empresa: ".$th];
+          echo json_encode($res);
+        }
+      }else{
+        //empresa no definida correctamente
+        $res = ["status"=>"error","mensaje"=>"No fue posible localizar la empresa."];
+        echo json_encode($res);
+      }
+      
+    } catch (\Throwable $th) {
+      $res = ["status"=>"error","mensaje"=>"No fue posible consultar los datos de la empresa: ".$th];
+      echo json_encode($res);
     }
-    echo "paso todo";
-    
+    // $queryEmp = mysqli_query($conexion, $sqlEmp);
   }
 }else{
 
