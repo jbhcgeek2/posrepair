713c0727b9816c40b69724ef129977598f97cc9a
jbhcgeek2 <joelbh92@gmail.com>
2024-05-11 12:25:51 -0700

    formulario de registro nuevo trabajo

--- assets/js/altaTrabajo.js
+++ assets/js/altaTrabajo.js
@@ -1,17 +1,19 @@
-
-Swal.fire({
-  title: 'Antes de continuar...',
-  text: 'Ya se encuentra registrado el cliente?',
-  icon: 'warning',
-  showDenyButton: true,
-  confirmButtonText: 'Ya esta registrado',
-  denyButtonText: 'No, Registrar nuevo cliente'
-}).then((result)=>{
-  if(result.isConfirmed){
-    //ya esta registrado
-
-  }else{
-    //no esta registrado, lo damos de alta
-    window.location = 'altaCliente.php';
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  Swal.fire({
+    title: 'Antes de continuar...',
+    text: 'Ya se encuentra registrado el cliente?',
+    icon: 'warning',
+    showDenyButton: true,
+    confirmButtonText: 'Ya esta registrado',
+    denyButtonText: 'No, Registrar nuevo cliente'
+  }).then((result)=>{
+    if(result.isConfirmed){
+      //ya esta registrado
+  
+    }else{
+      //no esta registrado, lo damos de alta
+      window.location = 'altaCliente.php';
+    }
+  })
+  
 })
