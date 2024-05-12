189597e29a59145d632aa991658c7e9ced50f7be
jbhcgeek2 <joelbh92@gmail.com>
2024-05-11 18:09:26 -0700

    se implementan micro servicios al fromulario de alta de trabajos

--- assets/js/altaTrabajo.js
+++ assets/js/altaTrabajo.js
@@ -16,4 +16,43 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   })
 
+  let tipoServ = document.getElementById('tipoServicio');
+  tipoServ.addEventListener('change', function(){
+    //buscaremos el precio sugerido del servicio
+    let datoServ = new FormData();
+    datoServ.append('servCheck',datoServ);
+
+    let envioServ = new XMLHttpRequest();
+    envioServ.open('POST','../includes/trabajosOperaciones.php', false);
+    envioServ.send(datoServ);
+
+    if(envioServ.status == 200){
+      let res = JSON.parse(envioServ.responseText);
+
+      if(res.status == "ok"){
+        //se consulto correctamente
+        document.getElementById('costoServicio').value = res.data;
+        
+      }else{
+        //error de operacion
+        let err = res.mensaje;
+        Swal.fire(
+          'Ha ocurrido un error',
+          'Verifica: '+err,
+          'error'
+        )
+      }
+    }else{
+      //error de servidor
+      Swal.fire(
+        'Servidor Inalcansable',
+        'Verifica tu conexion a internet',
+        'error'
+      )
+    }
+
+  })
+
 })
+
+
---
+++ includes/trabajosOperaciones.php
@@ -0,0 +1,51 @@
+<?php 
+  session_start();
+
+  if(!empty($_SESSION['usuarioPOS'])){
+    //se detecto la sesion, cargamos las funciones
+    include("empresas.php");
+    include("conexion.php");
+    include("usuarios.php");
+
+    $usuario = $_SESSION['usuarioPOS'];
+    $empresa = datoEmpresaSesion($usuario,"id");
+		$empresa = json_decode($empresa);
+		$idEmpresaSesion = $empresa->dato;
+
+    $dataUSer = getDataUser($idEmpresaSesion,$usuario);
+		$dataUSer = json_decode($dataUSer);
+		$idSucursalN = $dataUSer->sucursalID;
+
+    //verificamos la existencia de metodos
+    if(!empty($_POST['servCheck'])){
+      //verificamos la existencia y precio de un servicio
+      $idServ = $_POST['servCheck'];
+
+      $sql = "SELECT * FROM SERVICIOS WHERE empresaID = '$idEmpresaSesion' 
+      AND idServicio = '$idServ'";
+      try {
+        $query = mysqli_query($conexion, $sq);
+
+        if(mysqli_num_rows($query) == 1){
+          $fetch = mysqli_fetch_assoc($query);
+          $precio = "";
+          if($fetch['precioFijo'] == 1){
+            $precio = $fetch['precioFijo'];
+          }else{
+            $precio = "0";
+          }
+          $res = ['status'=>'ok','mensaje'=>'operationComplete','data'=>$precio];
+          echo json_encode($res);
+        }else{
+          //servicio no localizado
+          $res = ['status'=>'error','mensaje'=>'No se localizo el servicio'];
+          echo json_encode($res);
+        }
+      } catch (\Throwable $th) {
+        //error de consulta BD
+        $res = ['status'=>'error','mensaje'=>'Ha ocurrido un error al consultar el servicio'];
+        echo json_encode($res);
+      }
+    }
+  }
+?>
