152e5047bb1d820579d2ea8aa9611ca4fc4b2056
jbhcgeek2 <joelbh92@gmail.com>
2024-05-15 13:29:44 -0700

    Correccion de validacion en JS

--- verInfoTrabajo.php
+++ verInfoTrabajo.php
@@ -95,19 +95,19 @@ session_start();
                           <input type="text" id="clienteTrabajo" class="form-control" value="<?php echo $nombreCliente; ?>" readonly>
                         </div>
 
-                        <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+                        <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
                           <label for="fechaEntrega" class="form-label">Fecha de Entrega</label>
                           <input type="date" name="fechaEntrega" id="fechaEntrega" class="form-control" required
                           value="<?php echo $fechaEntrega; ?>">
                         </div>
 
-                        <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+                        <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
                           <label for="costoServicio" class="form-label">Costo</label>
                           <input type="number" name="costoServicio" id="costoServicio" class="form-control" 
                           value="<?php echo $costoIni; ?>" readonly>
                         </div>
 
-                        <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+                        <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
                           <label for="anticipoServicio" class="form-label">Anticipo</label>
                           <input type="number" name="anticipoServicio" id="anticipoServicio" class="form-control" 
                           value="<?php echo $anticipo; ?>" readonly>
