d6ff38323e2f0fbe0c23b4febddf4d63679ebe57
jbhcgeek2 <joelbh92@gmail.com>
2024-05-20 11:22:14 -0700

    Se crea pagina para ver trabajos activos

--- assets/js/verTrabajos.js
+++ assets/js/verTrabajos.js
@@ -19,10 +19,33 @@ estatus.addEventListener('change', function(){
       let contenido = '';
       if(res.mensaje == "dataOk"){
         //si tiene resultados
-        
+        let colorStatus = {"Activo":'badge rounded-pill text-bg-success',
+        "En Proceso":'badge rounded-pill text-bg-warning',
+        "En Espera":'badge rounded-pill text-bg-secondary',
+        "Finalizado":'badge rounded-pill text-bg-danger',
+        "Cancelado":'badge rounded-pill text-bg-dark'};
+
         for (let z = 0; z < res.data.length; z++) {
           console.log(res.data[z].numTrabajo);
-          
+          let nombreCli = res.data[z].nombreCliente;
+          let tipoTra = res.data[z].nombreServicio;
+          let fechaReg = res.data[z].fechaTrabajo;
+          let fechaEnt = res.data[z].fechaEntrega;
+          let estatusTra = res.data[z].estatusTrabajo;
+          let trabId = res.data[z].idTrabajo;
+
+          let colorEstatus = colorStatus[estatusTra];
+
+          contenido = contenido+`<tr>
+            <td>${nombreCli}</td>
+            <td>${tipoTra}</td>
+            <td>${fechaReg}</td>
+            <td>${fechaEnt}</td>
+            <td><span class='${colorEstatus}'>${estatusTra}</span></td>
+            <td>
+              <a href='verInfoTrabajo.php?data=${trabId}' class='btn btn-primary'>Ver</a>
+            </td>
+          </tr>`;
         }
       }else{
         //no se encontraron resultados
