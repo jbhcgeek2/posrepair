05191154dfa6f525cd9d779799313bff3d5c72c3
jbhcgeek2 <joelbh92@gmail.com>
2024-05-21 15:56:23 -0700

    creacion de pagina saldos

--- saldos.php
+++ saldos.php
@@ -34,14 +34,15 @@ session_start();
 			    
 			    
           <div class="col-6 col-lg-3">
-                    <div class="app-card app-card-stat shadow-sm h-100">
-                      <div class="app-card-body p-3 p-lg-4">
-                        <h4 class="stats-type mb-1">Saldo Actual</h4>
-                        <div class="stats-figure">$0.00</div>
-                        <a class="app-card-link-mask" href="#"></a>
-                    </div><!--//app-card-->
-                  </div><!--//col-->
-          <hr class="my-4">
+            <div class="app-card app-card-stat shadow-sm h-100">
+              <div class="app-card-body p-3 p-lg-4">
+                <h4 class="stats-type mb-1">Saldo Actual</h4>
+                <div class="stats-figure">$0.00</div>
+                <a class="app-card-link-mask" href="#"></a>
+            </div><!--//app-card-->
+          </div><!--//col-->
+        </div>
+          
         
 
 
