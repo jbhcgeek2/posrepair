62d6ea8f9edc73f10303c298021b8cda6e27d3a7
jbhcgeek2 <joelbh92@gmail.com>
2024-05-21 11:03:47 -0700

    Reporte ventas por usuario

--- ventasUsuario.php
+++ ventasUsuario.php
@@ -90,8 +90,8 @@ session_start();
 
                     <hr clas="my-4">
 
-                    <h5>Articulos Vendidos: <span class='success-text' id='totArti'></span> </h5>
-                    <h5>Total de Venta: <span class='success-text' id='totVenta'></span></h5>
+                    <h5>Articulos Vendidos: <span class='stext-success fw-bold' id='totArti'></span> </h5>
+                    <h5>Total de Venta: <span class='text-success fw-bold' id='totVenta'></span></h5>
 
                     <div class="row" style="height:600px;overflow-y:scroll;">
                       <table class="table">
