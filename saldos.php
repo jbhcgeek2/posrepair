34c79cf336cf397ee9e7b5dd32997280be3f4a42
jbhcgeek2 <joelbh92@gmail.com>
2024-05-23 10:47:18 -0700

    creacion de pagina saldos

--- saldos.php
+++ saldos.php
@@ -50,26 +50,28 @@ session_start();
 			    
 			    <h1 class="app-page-title">Saldos de la cuenta</h1>
 			    
-			    
-          <div class="col-6 col-lg-3 mb-3">
-            <div class="app-card app-card-stat shadow-sm h-100">
-              <div class="app-card-body p-3 p-lg-4">
-                <h4 class="stats-type mb-1">Saldo Actual</h4>
-                <div class="stats-figure fw-bold">$<?php echo number_format($saldoTotal,2); ?></div>
-                <a class="app-card-link-mask" href="#"></a>
-              </div><!--//app-card-->
-            </div><!--//col-->
-          </div>
+			    <div class="row">
+            <div class="col-6 col-lg-3 mb-3">
+              <div class="app-card app-card-stat shadow-sm h-100">
+                <div class="app-card-body p-3 p-lg-4">
+                  <h4 class="stats-type mb-1">Saldo Actual</h4>
+                  <div class="stats-figure fw-bold">$<?php echo number_format($saldoTotal,2); ?></div>
+                  <a class="app-card-link-mask" href="#"></a>
+                </div><!--//app-card-->
+              </div><!--//col-->
+            </div>
 
-          <div class="col-6 col-lg-3 mb-3">
-            <div class="app-card app-card-stat shadow-sm h-100">
-              <div class="app-card-body p-3 p-lg-4">
-                <h4 class="stats-type mb-1">Saldo Sucursales</h4>
-                <div class="stats-figure fw-bold">$<?php echo number_format($saldoTotal,2); ?></div>
-                <a class="app-card-link-mask" href="#"></a>
-              </div><!--//app-card-->
-            </div><!--//col-->
+            <div class="col-6 col-lg-3 mb-3">
+              <div class="app-card app-card-stat shadow-sm h-100">
+                <div class="app-card-body p-3 p-lg-4">
+                  <h4 class="stats-type mb-1">Saldo Sucursales</h4>
+                  <div class="stats-figure fw-bold">$<?php echo number_format($saldoTotal,2); ?></div>
+                  <a class="app-card-link-mask" href="#"></a>
+                </div><!--//app-card-->
+              </div><!--//col-->
+            </div>
           </div>
+          
 
         <div class="row">
           <div class="col-sm-12">
