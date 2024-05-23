bbf7610e5cc57767ed1e2ff039cf2c517517cc7b
jbhcgeek2 <joelbh92@gmail.com>
2024-05-23 10:45:18 -0700

    creacion de pagina saldos

--- saldos.php
+++ saldos.php
@@ -59,6 +59,15 @@ session_start();
                 <a class="app-card-link-mask" href="#"></a>
             </div><!--//app-card-->
           </div><!--//col-->
+
+          <div class="col-6 col-lg-3 mb-3">
+            <div class="app-card app-card-stat shadow-sm h-100">
+              <div class="app-card-body p-3 p-lg-4">
+                <h4 class="stats-type mb-1">Saldo Sucursales</h4>
+                <div class="stats-figure fw-bold">$<?php echo number_format($saldoTotal,2); ?></div>
+                <a class="app-card-link-mask" href="#"></a>
+            </div><!--//app-card-->
+          </div><!--//col-->
         </div>
 
         <div class="row">
