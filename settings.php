24dca02f2bb95b2f4c8707cc447fe137d4b4f4b2
jbhcgeek2 <joelbh92@gmail.com>
2024-06-03 13:12:00 -0700

    Implementacion de politicas y condiciones

--- settings.php
+++ settings.php
@@ -189,6 +189,35 @@ session_start();
 					        </div><!--//app-card-body-->
 				        </div><!--//app-card-->
 			        </div><!--//col-->
+
+
+
+
+              <div class="col-12 col-lg-12">
+				        <div class="app-card app-card-chart h-100 shadow-sm">
+					        <div class="app-card-header p-3">
+						        <div class="row justify-content-between align-items-center">
+
+							        <div class="col-auto">
+						            <h4 class="app-card-title"></h4>
+							        </div><!--//col-->
+
+							        <div class="col-auto">
+								        <div class="card-header-action">
+									        <a href="account.php">Mis Datos</a>
+								        </div><!--//card-header-actions-->
+							        </div><!--//col-->
+
+						        </div><!--//row-->
+					        </div><!--//app-card-header-->
+                  <div class="app-card-body p-3 p-lg-4">
+
+                  asdfasd
+					        
+
+					        </div><!--//app-card-body-->
+				        </div><!--//app-card-->
+			        </div><!--//col-->
           <hr class="my-4">
         
 
