f6a09bf7e207e9b8a7acc9b15ac8bbea95ca0bd8
jbhcgeek2 <joelbh92@gmail.com>
2024-06-04 12:06:50 -0700

    Implementacion de generador de codigos en alta de productos

--- verInfoProducto.php
+++ verInfoProducto.php
@@ -32,7 +32,7 @@ session_start();
       }
 
       // $rutaBarcode = php-barcode-master/barcode.php?text='.$codigoBar.'&codetype=codebar&orientation=horizontal
-      $imagenCodigo = '<img src="php-barcode-master/barcode.php?text='.$codigoBar.'&codetype=Code39&orientation=horizontal&size=50">';
+      $imagenCodigo = '<img src="../php-barcode-master/barcode.php?text='.$codigoBar.'&codetype=Code39&orientation=horizontal&size=50">';
       
       if($datosProd->status == "ok"){
         $error = "no";
