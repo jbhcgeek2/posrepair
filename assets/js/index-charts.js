5a7892e3d6f9110b641424f166c6bd050ec73a49
jbhcgeek2 <joelbh92@gmail.com>
2024-06-05 16:18:49 -0700

    grafica

--- assets/js/index-charts.js
+++ assets/js/index-charts.js
@@ -247,7 +247,54 @@ var lineChartConfig = {
 // }
 
 
+// Servicios mas vendidos
 
+const configBar = {
+  type: 'bar',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart'
+      }
+    }
+  },
+};
+
+const DATA_COUNT = 7;
+const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Fully Rounded',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.red,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+      borderWidth: 2,
+      borderRadius: Number.MAX_VALUE,
+      borderSkipped: false,
+    },
+    {
+      label: 'Small Radius',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.blue,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+      borderWidth: 2,
+      borderRadius: 5,
+      borderSkipped: false,
+    }
+  ]
+};
+
+// Fin servicios mas vendidos
 
 
 
@@ -257,6 +304,9 @@ window.addEventListener('load', function(){
 	
 	var lineChart = document.getElementById('canvas-linechart').getContext('2d');
 	window.myLine = new Chart(lineChart, lineChartConfig);
+
+	var lineChart2 = document.getElementById('canvas-linechart2').getContext('2d');
+	window.myLine = new Chart(lineChart2, configBar);
 	
 	// var barChart = document.getElementById('canvas-barchart').getContext('2d');
 	// window.myBar = new Chart(barChart, barChartConfig);
