26e489d16334da55bfe1f3a0860834c2e7f2ca97
jbhcgeek2 <joelbh92@gmail.com>
2024-06-21 16:28:49 -0700

    Metodo para Chips

--- verInfoProducto.php
+++ verInfoProducto.php
@@ -121,7 +121,7 @@ session_start();
                                           <input type="text" id="codigoChip" class="form-control" onchange="insertaChip()">
                                         </div>
 
-                                        <div class="col-sm-12 col-md-6 offset-md-3" id="resChips" style="text-align:center"></div>
+                                        <div class="col-sm-12 col-md-6 offset-md-3" id="resChips" style="text-align:center;height:200px;overflow-y:scroll;"></div>
                                       </div><!--fin row modal-body-->
 
                                     </div><!--Fin modal body-->
