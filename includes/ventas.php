2a7bacc9145e167ce16da4153b148f81f6f34a09
jbhcgeek2 <joelbh92@gmail.com>
2024-07-07 13:26:59 -0700

    Correccion ticket 2 o mas servicios

--- includes/ventas.php
+++ includes/ventas.php
@@ -89,9 +89,9 @@ function verTicket($ticket,$idUsuario){
             //es trabajo/servicio, consultamos el nombre del servicio para agregarlo al ticket
             $sqlExt3 = "SELECT a.*,c.nombreServicio FROM DETALLEVENTA a INNER JOIN TRABAJOS b ON a.trabajoID = b.idTrabajo 
             INNER JOIN SERVICIOS c ON b.servicioID = c.idServicio 
-            WHERE a.ventaID = '$ticket'";
+            WHERE a.ventaID = '$ticket' LIMIT 1";
             $queryExt3 = mysqli_query($conexion, $sqlExt3);
-            $fetchExt3 = mysqli_fetch_array($queryExt3);
+            $fetchExt3 = mysqli_fetch_assoc($queryExt3);
             $data2[$x] = $fetchExt3;
             print_r($fetchExt3);
             // $auxX = $x;
