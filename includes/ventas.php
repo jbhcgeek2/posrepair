c89da885fb2f2ac69d9eb9552146ca9f1adcda52
jbhcgeek2 <joelbh92@gmail.com>
2024-07-07 12:59:26 -0700

    Correccion ticket 2 o mas servicios

--- includes/ventas.php
+++ includes/ventas.php
@@ -91,17 +91,17 @@ function verTicket($ticket,$idUsuario){
             INNER JOIN SERVICIOS c ON b.servicioID = c.idServicio 
             WHERE a.ventaID = '$ticket'";
             $queryExt3 = mysqli_query($conexion, $sqlExt3);
-
-            if(mysqli_num_rows($queryExt3) > 1){
+            $auxX = $x;
+            // if(mysqli_num_rows($queryExt3) > 1){
               while($fetchExt3 = mysqli_fetch_assoc($queryExt3)){
-                $data2[$x] = $fetchExt3;
-                $x++;
+                $data2[$auxX] = $fetchExt3;
+                $auxX++;
               }//fin del while tickets
 
-            }else{
-              $fetchExt3 = mysqli_fetch_assoc($queryExt3);
-              $data2[$x] = $fetchExt3;
-            }
+            // }else{
+            //   $fetchExt3 = mysqli_fetch_assoc($queryExt3);
+            //   $data2[$x] = $fetchExt3;
+            // }
 
           }
           // $data2[$x] = $fetch2;
