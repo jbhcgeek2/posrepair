3c8b139d835953f771db151015de7a588d1b48e0
jbhcgeek2 <joelbh92@gmail.com>
2024-07-16 14:56:49 -0700

    Correccion de ventas del dia

--- ventasDelDia.php
+++ ventasDelDia.php
@@ -139,7 +139,7 @@ session_start();
                                 $nombreCosa = "";
                                 $fechaVenta = $fetch['fechaVenta'];
                                 //verificamos si la venta es un producto o servicio
-                                if($fetch['articuloID']>0){
+                                if($fetch['articuloID'] > 1){
                                   //se trata de un articulo
                                   $idProd = $fetch['articuloID'];
                                   $sqlExt = "SELECT * FROM ARTICULOS WHERE idArticulo = '$idProd' AND empresaID = '$idEmpresaSesion'";
