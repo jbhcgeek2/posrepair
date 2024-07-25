2e2f18f87848b62ba470bc5f07f7c6cfe7db7e1a
jbhcgeek2 <joelbh92@gmail.com>
2024-07-24 21:45:38 -0700

    Proceso de auditoria

--- assets/js/verAuditorias.js
+++ assets/js/verAuditorias.js
@@ -2,9 +2,11 @@ let btnNuevaAudi = document.getElementById('newAuditoria');
 btnNuevaAudi.addEventListener('click', function(){
   Swal.fire({
     title: 'Generar nueva Auditoria',
+    text: 'Ingresa la clave de autorizacion',
     input: 'password',
     showCancelButton: true,
-    confirmButtonTExt: 'Generar'
+    backdrop: true,
+    confirmButtonText: 'Generar'
   }).then((result)=>{
     console.log(result);
     if(result.isConfirmed){
