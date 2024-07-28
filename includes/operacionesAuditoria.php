4ed738ee67941e4c1b1a60835a94fd33897aa133
jbhcgeek2 <joelbh92@gmail.com>
2024-07-27 21:58:37 -0700

    Proceso de auditoria

--- includes/operacionesAuditoria.php
+++ includes/operacionesAuditoria.php
@@ -36,20 +36,85 @@ if(!empty($_SESSION['usuarioPOS'])){
       echo $claveAutori;
       $claveInput = $_POST['autorization'];
       //consultamos la clave de la empresa
-
+      $fecha = date('Y-m-d');
+      $hora = date('H:i:s');
       if($claveInput == $claveAutori){
-        //se autoriza la auditoria
-        //primero consultaremos los articulos y las existencias actuales
-        //y los amacenaremos en una tabla
-        $sql = "SELECT * FROM ARTICULOS WHERE empresaID = '$idEmpresaSesion' AND 
-        estatusArticulo = '0'";
+        //se autoriza la auditoria, generamos un nuevo registro
+        $sqlX = "INSERT INTO AUDITORIAS (fechaInicio,
+        usuarioInicia,empresaID,horaInicio) VALUES 
+        ('$fecha','$usuario','$idEmpresaSesion','$hora')";
         try {
-          $query = mysqli_query($conexion, $sql);
-          while($fetch = mysqli_fetch_assoc($query)){
-            echo $fetch['nombreArticulo'];
-          }//fin del while
+          $queryX = mysqli_query($conexion, $sqlX);
+          $idAuditoria = mysqli_insert_id($conexion);
+          //primero consultaremos los articulos y las existencias actuales
+          //y los amacenaremos en una tabla
+          $sql = "SELECT * FROM ARTICULOS WHERE empresaID = '$idEmpresaSesion' AND 
+          estatusArticulo = '0'";
+          //consultamos las sucursales de la empresa, para ver las existencias
+          $sql2 = "SELECT * FROM SUCURSALES WHERE empresaSucID = '$idEmpresaSesion' AND
+          estatusSuc = '1'";
+          try {
+            $query = mysqli_query($conexion, $sql);
+            $query2 = mysqli_query($conexion, $sql2);
+            $sucursales = "";
+            while($fetch2 = mysqli_fetch_assoc($query2)){
+              $idSuc = $fetch2['idSucursal'];
+              if($sucursales == ""){
+                $sucursales = $idSuc;
+              }else{
+                $sucursales .= ",".$idSuc;
+              }
+            }//fin del while sucursales
+            //a continuacion hacemos el while de productos
+            while($fetch = mysqli_fetch_assoc($query)){
+              //consultamos la existencia en las sucursales
+              $idArticulo = $fetch['idArticulo'];
+              $existenciaArticulo = "";
+              $sql3 = "SELECT * FROM ARTICULOSUCURSAL WHERE articuloID = '$idArticulo' 
+              AND sucursalID IN ($sucursales)";
+              try {
+                $query3 = mysqli_query($conexion,$sql3);
+                while($fetch3 = mysqli_fetch_assoc($query3)){
+                  $existen = $fetch3['existenciaSucursal'];
+                  $idSucArti = $fetch3['sucursalID'];
+
+                  $datoExiste = $idSucArti."=".$existen;
+                  if($existenciaArticulo == ""){
+                    $existenciaArticulo = $datoExiste;
+                  }else{
+                    $existenciaArticulo = "|".$datoExiste;
+                  }
+                }//fin del while articulo sucursal
+
+                $nombreArti = $fetch['nombreArticulo'];
+                $proveedor = $fetch['proveedorID'];
+                $chip = $fetch['esChip'];
+                $categoria = $fetch['categoriaID'];
+                //ahora insertamos la informacion del articulo en la nueva tabla
+                $sql4 = "INSERT INTO ARTICULOAUDITORIA (nombreArticuloAudi,
+                idArticuloAudi,existenciasAudi,proveedorAudi,empresaID,auditoriaID,
+                esChipAudi,categoriaArtiAudi) VALUES ('$nombreArti','$idArticulo',
+                '$existenciaArticulo','$proveedor','$idEmpresaSesion','$idAuditoria',
+                '$chip','$categoria')";
+                try {
+                  $query4 = mysqli_query($conexion, $sql4);
+                } catch (\Throwable $th) {
+                  //no se inserto el registro de auditoria de objetoi
+                }
+              } catch (\Throwable $th) {
+                $datoExiste = "error";
+              }
+
+            }//fin del while articulos
+          } catch (\Throwable $th) {
+            //error al consultar los articulos
+            $res = ['status'=>'error','mensaje'=>'Error al consultar los articulos: '.$th];
+            echo json_encode($res);
+          }
         } catch (\Throwable $th) {
-          //throw $th;
+          //no se pudo generar la auditoria
+          $res = ['status'=>'error','mensaje'=>'No se pudo generar la auditoria'];
+          echo json_encode($res);
         }
       }else{
         $res = ['status'=>'error','mensaje'=>'Clave de autorizacion incorrecta.'];
