eb556bf5e646a96b70c5c7e7ea0a0d1122a1cc2c
jbhcgeek2 <joelbh92@gmail.com>
2024-08-27 15:53:22 -0700

    Reporte refacciones vendidas

--- refaccionesVendidas.php
+++ refaccionesVendidas.php
@@ -157,7 +157,7 @@ session_start();
     <!-- Page Specific JS -->
     <script src="assets/js/app.js"></script> 
     <script src="assets/js/swetAlert.js"></script>
-    <script src="assets/js/articulosVendidos.js"></script>
+    <script src="assets/js/refaccionesVendidas.js"></script>
 </body>
 </html> 
 
