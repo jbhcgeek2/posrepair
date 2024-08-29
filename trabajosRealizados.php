52c0b15c6ea4875518394dc5e77dd628c341fffe
jbhcgeek2 <joelbh92@gmail.com>
2024-08-28 17:28:05 -0700

    Modificacion reportes x

--- trabajosRealizados.php
+++ trabajosRealizados.php
@@ -121,8 +121,8 @@ session_start();
                                 }//fin del while articulos agrupados
                                 echo "<tr>
                                   <td colspan='2' style='text-align:right;'>TOTAL</td>
-                                  <td style='text-align:left;'><strong>".number_format($suma,2)."</strong></td>
-                                  <td></td>
+                                  <td style='text-align:left;'><strong>$".number_format($suma,2)."</strong></td>
+                                  <td colspan='2'></td>
                                 </tr>";
                               }else{
                                 //sin articulos vendidos
