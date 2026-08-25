e8a41ac1a63fe5f1c5d9f5e930fb9e3ebf95421a
jbhcgeek2 <joelbh92@gmail.com>
2026-08-24 17:14:55 -0700

    Correewccion consulta de productos en Caja

--- ventasDelDia.php
+++ ventasDelDia.php
@@ -206,6 +206,7 @@ session_start();
                                   </td>
                                 </tr>";
                               }//fin del while
+                              mysqli_close($conexion);
                               echo "<tr>
                               <td colspan='3' class='fw-bold' style='text-align:right'>Subtotal</td>
                               <td class='fw-bold'>$".number_format($totalVenta,2)."</td>
